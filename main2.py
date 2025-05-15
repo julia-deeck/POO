f9a478b8688f97652410898d8a8e54c3e3c33a97
Júlia <julia.chdck@gmail.com>
2025-05-15 16:35:37 -0300

    class Animal

---
+++ main2.py
@@ -0,0 +1,17 @@
+from biblioteca import *
+
+gato = Gato("Peludinho", "Laranja")
+gato.miar()
+gato.comer()
+
+cachorro = Cachorro("Tafarel", "Branco")
+cachorro.latir()
+cachorro.comer()
+
+coelho = Coelho("Nuvem","Branco")
+coelho.chiar()
+coelho.comer()
+
+vaca = Vaca("Lily", "Malhada")
+vaca.mugir()
+vaca.comer()
